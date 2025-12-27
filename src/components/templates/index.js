bb34d50d68de5279016b76e6459d2a15b8411266
SK <lim_swee_kiew@rp.edu.sg>
2025-12-27 11:33:54 +0800

    load

--- src/components/templates/index.js
+++ src/components/templates/index.js
@@ -2,7 +2,6 @@
 // When templates are loaded, their name will be compared to the keys of the
 // exported object and that template will be used. If a template cannot be
 // found, the default template will be used instead.
-
 // TODO: Use dynamic loading to prevent all templates to be loaded at once.
 import DefaultTemplate from "./default";
 import CustomTemplate from "./customTemplate";
